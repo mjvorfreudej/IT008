1fe1f816bd56118604594e5d349f2ebe2804cc58
mjvorfreudej <24521498@gm.uit.edu.vn>
2025-10-22 22:34:11 +0700

    add practice 01

---
+++ BTH1_LamDoanHaQuyen_24521498/Bai02/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bai02
+{
+    class Program
+    {
+        const int MAX = 10000000;
+        static long[] primeSum = new long[MAX + 1];
+
+        //Sàng Eratosthenes để đánh dấu các số nguyên tố
+        static void Precompute()
+        {
+            bool[] isPrime = new bool[MAX + 1];
+            for (int i = 2; i <= MAX; i++)
+                isPrime[i] = true;
+
+            for (int i = 2; i * i <= MAX; i++) {
+                if (isPrime[i]) {
+                    for (int j = i * i; j <= MAX; j += i)
+                        isPrime[j] = false;
+                }
+            }
+
+            long sum = 0;
+            for (int i = 2; i <= MAX; i++) {
+                if (isPrime[i])
+                    sum += i;
+                primeSum[i] = sum;
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            Precompute();
+            Console.Write("Nhap so nguyen duong n: ");
+            int n = int.Parse(Console.ReadLine());
+            if (n > MAX) n = MAX;
+            Console.WriteLine($"Tong cac so nguyen to nho hon {n} la: {primeSum[n]}");
+        }
+    }
+}
